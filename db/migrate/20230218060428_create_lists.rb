1d1886129ad752fa289a2afb077fcf4147be0dac
Yi Suan Lee <l.yixuane@gmail.com>
2023-02-18 14:05:47 +0800

    Added Movie, Bookmark and List models

---
+++ db/migrate/20230218060428_create_lists.rb
@@ -0,0 +1,10 @@
+class CreateLists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :lists do |t|
+      t.string :name
+
+      t.timestamps
+    end
+    add_index :lists, :name, unique: true
+  end
+end
